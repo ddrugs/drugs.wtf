ea920402d2375fce7fd5abf8dcdc18e3b3136145
ddrugs <kean.titameng@gmail.com>
2026-04-26 13:07:31 +0800

    Update and rename noway2.lua to drugs.wtf.lua

---
+++ drugs.wtf.lua
@@ -0,0 +1,5 @@
+this is not the script, dont even try to crack it.
+
+ive spent over 100 hours on this, of course i need the money because all that hard work? just to make it free? 
+
+This cheat is better than glory thats why i made it pricey.
